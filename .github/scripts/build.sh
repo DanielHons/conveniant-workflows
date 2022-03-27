e72f1ebae885fecf50d9ef596305f5899f47c5f0
Daniel Hons <mail@danielhons.de>
2022-03-27 23:53:41 +0200

    feat: Extract scripts

---
+++ .github/scripts/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "Your build script is empty. Version $VERSION can not be build"
---
+++ .github/scripts/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "Your test script is empty. Nothing was tested here"
